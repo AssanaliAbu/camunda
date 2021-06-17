68bde3aa530b9e2a2a671833952276a879b2ed44
Assanali Abu <assanali.abu@nu.edu.kz>
2021-06-18 04:00:18 +0600

    Updates for Microservices task

--- src/main/java/com/example/workflow/CalculatorRemainder.java
+++ src/main/java/com/example/workflow/CalculatorRemainder.java
@@ -1,20 +1,31 @@
 package com.example.workflow;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.stereotype.Component;
 
+import java.util.LinkedHashMap;
+import java.util.List;
 
-    @Component
+
+@Component
+    @EnableFeignClients
     public class CalculatorRemainder implements JavaDelegate {
+        @Autowired
+        FeignInterface feignInterface;
 
         @Override
         public void execute(DelegateExecution delegateExecution) throws Exception {
-            int num1 = (int) delegateExecution.getVariable("num1");
-            int num2 = (int) delegateExecution.getVariable("num2");
-            int res = num1%num2;
-            delegateExecution.setVariable("result", res);
+            String name = (String) delegateExecution.getVariable("name");
+            List<LinkedHashMap> res = feignInterface.findbyname(name);
+            delegateExecution.setVariable("age", res.get(0).get("age"));
+
 
         }
+
     }
 
